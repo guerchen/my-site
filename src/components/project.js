a194c66eaaeb3efc5d98117f6410e43f9ac93c48
guerchen <ariel.guerchen.10@gmail.com>
2022-12-04 17:55:11 -0300

    New projects page

---
+++ src/components/project.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import './project.css';
+
+class Project extends React.Component {
+    constructor(props) {
+        super(props);
+      }
+    render () {
+        return (
+        <div className='project-box'>
+            <div className='title-div'>
+                <h3 className='post-title'>{this.props.name}</h3>
+            </div>
+            <div className='content-div'>
+                <p className='post-content'>{this.props.description}</p>
+            </div>
+            <a href={this.props.link} target="_blank" rel="noopener noreferrer">Further info</a>
+        </div>)
+    }
+
+  }
+  
+  export {Project};
--- src/pages/projects.js
+++ src/pages/projects.js
@@ -1,16 +1,24 @@
 import React from 'react';
 import './projects.css';
+import { Project } from '../components/project'; 
 import { Footer } from '../components/footer'
 
 function Projects() {
     return (
         <div className='content'>
             <div className='projects-card'>
-                [WIP] projects:
-                1. This site
-                2. Streamlit waves
-                3. My own facematch liveness
-                4. 
+                <Project name='This very site' description='My first personal website, made using React.js and deployed to github pages.
+                Learned a lot about React.js components, github pages deployment, and css styling.'
+                link='https://github.com/guerchen/my-site' />
+                <Project name="Potential for wave energy in Brazil" description="This streamlit project was first motivated by a 
+                university project where we had to research the potential energy generation for unusual forms of renewable energy. I applied
+                a research paper based optimized computational method to a database of Brazil's historical wave measurements accross a variety
+                of locations."
+                link='https://guerchen-waveenergy-waves-apklz5.streamlit.app/' />
+                <Project name='My own Facematch/Liveness' description='With the goal of better understanding the products offered by idwall,
+                I sought out to build my own version of Facematch with Liveness using a Kaggle selfies and IDs dataset and Tensorflow and Keras.
+                I learned a lot about image preprocessing, convolutional neural networks, TPUs, and more!'
+                link='https://github.com/guerchen/my-own-facematch-liveness/blob/37dde117d7252f8b9da68add5aaf7f77b45829d4/my-own-facematch-liveness.ipynb' />
             </div>
             <Footer />
         </div>
